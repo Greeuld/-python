3ea1ccbe1e8aaa55fc596ea779852a9b9a46aef1
Greeuld <66782512+Greeuld@users.noreply.github.com>
2020-06-16 14:56:54 +0300

    Add files via upload

---
+++ L2_E2.py
@@ -0,0 +1,19 @@
+# Для списка реализовать обмен значений соседних элементов, т.е.
+# Значениями обмениваются элементы с индексами 0 и 1, 2 и 3 и т.д.
+# При нечетном количестве элементов последний сохранить на своем месте.
+# Для заполнения списка элементов необходимо использовать функцию input().
+
+quan = int(input("Enter the number of items: "))
+
+my_list = []
+i = 0
+while i < quan:
+    my_list.append(input("Enter the following item value: "))
+    i += 1
+
+el = 0
+for elem in range(int(len(my_list) / 2)):
+    my_list[el], my_list[el + 1] = my_list[el + 1], my_list[el]
+    el += 2
+
+print(my_list)
---
+++ L2_E6.py
@@ -0,0 +1,27 @@
+# Реализовать структуру данных «Товары».
+# Она должна представлять собой список кортежей.
+# Каждый кортеж хранит информацию об отдельном товаре.
+# В кортеже должно быть два элемента — номер товара и словарь с параметрами
+# (характеристиками товара: название, цена, количество, единица измерения).
+# Структуру нужно сформировать программно, т.е. запрашивать все данные у пользователя.
+
+goods = []
+features = {'name': '', 'price': '', 'quantity': '', 'unit': ''}
+analytics = {'name': [], 'price': [], 'quantity': [], 'unit': []}
+num = 0
+feature_ = None
+control = None
+while True:
+    control = input("For quit press 'Q', for continue press 'Enter', for analytics press 'A'").upper()
+    if control == 'Q':
+        break
+    num += 1
+    if control == 'A':
+        print(f'\n Current analytics: ')
+        for key, value in analytics.items():
+            print(f'{key[:10]:>18}: {value}')
+    for f in features.keys():
+        feature_ = input(f'Input feature "{f}"')
+        features[f] = int(feature_) if (f == 'price' or f == 'quantity') else feature_
+        analytics[f].append(features[f])
+    goods.append((num, features))
